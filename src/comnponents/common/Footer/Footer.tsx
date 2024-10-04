37304ef0884b5bddff8d82c116328410b6041fe8
serhatbek <serhat.bekk@gmail.com>
2024-10-04 15:58:12 +0300

    ✨ footer component

--- src/comnponents/common/Footer/Footer.tsx
+++ src/comnponents/common/Footer/Footer.tsx
@@ -1,5 +1,14 @@
+import styles from "./styles.module.scss";
+const { footerContainer } = styles;
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className={footerContainer}>
+        @ 2024 Our eCom. All right reserved.
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,15 +1,15 @@
 import { Container } from "react-bootstrap";
 import styles from "./styles.module.scss";
-import { Header } from "../../comnponents/common";
+import { Footer, Header } from "../../comnponents/common";
 
 const { container, wrapper } = styles;
 
 const MainLayout = () => {
   return (
     <Container className={container}>
-      <div className={wrapper}>
-        <Header />
-      </div>
+      <Header />
+      <div className={wrapper}></div>
+      <Footer />
     </Container>
   );
 };
