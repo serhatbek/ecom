57614dd2092a34a4102c1f35673e62254063e20a
serhatbek <serhat.bekk@gmail.com>
2024-10-13 22:44:24 +0300

    ✨ actGetCategories async thunk

---
+++ src/store/categories/actions/actGetCategories.ts
@@ -0,0 +1,25 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+type TResponse = { id: number; title: string; prefix: string; img: string }[];
+
+const actGetCategories = createAsyncThunk(
+  "categories/actGetCategories",
+  async (_, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI;
+    try {
+      const response = await axios.get<TResponse>(
+        "http://localhost:5005/categories"
+      );
+      return response.data;
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        return rejectWithValue(error?.response?.data.message || error.message);
+      } else {
+        return rejectWithValue("An unexpected error...");
+      }
+    }
+  }
+);
+
+export default actGetCategories;
--- src/store/store.ts
+++ src/store/store.ts
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import categoriesReducer from "./categories/categorySlice";
 
 export const store = configureStore({
-  reducer: { categoriesReducer },
+  reducer: { categories: categoriesReducer },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
