4d86c66e692081549ed1488f5d309bab746c8e0a
serhatbek <serhat.bekk@gmail.com>
2024-10-15 14:14:30 +0300

    ✨ promise lifecycle actions

--- src/store/categories/categorySlice.ts
+++ src/store/categories/categorySlice.ts
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import actGetCategories from "./actions/actGetCategories";
 
 interface ICategoryState {
   records: { id: number; title: string; prefix: string; img: string }[];
@@ -16,6 +17,25 @@ const categoriesSlice = createSlice({
   name: "categories",
   initialState: initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(actGetCategories.pending, (state) => {
+      state.loading = "pending";
+      state.error = null;
+    });
+
+    builder.addCase(actGetCategories.fulfilled, (state, action) => {
+      state.loading = "succeeded";
+      state.records = action.payload;
+    });
+
+    builder.addCase(actGetCategories.rejected, (state, action) => {
+      state.loading = "failed";
+      if (action.payload && typeof action.payload === "string") {
+        state.error = action.payload;
+      }
+    });
+  },
 });
 
+export { actGetCategories };
 export default categoriesSlice.reducer;
